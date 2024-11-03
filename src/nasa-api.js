56001d57a23c7a73ca004e2e28364ca1eb896ebd
박시은 <charm516@naver.com>
2024-11-03 12:05:02 +0900

    [feat] - Add NASA API to APOD section on main page

--- src/nasa-api.js
+++ src/nasa-api.js
@@ -2,5 +2,13 @@ import { api_key } from "./apikey.js";
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
   .then((res) => res.json())
-  .then((data) => console.log(data))
+
+  .then((data) => {
+    console.log("data", data);
+    document.querySelector(".apod-text-box figure img").src = data.url;
+    document.querySelector(".apod-text-box figure img").alt = data.title;
+    document.querySelector(".apod-article h2").textContent = data.title;
+    document.querySelector(".apod-article-explanation ").textContent =
+      data.explanation;
+  })
   .catch((error) => console.error(error));
