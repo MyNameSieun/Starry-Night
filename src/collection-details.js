40741037c8d9ec55f8397247d7f9a90f9b7986f1
박시은 <charm516@naver.com>
2024-11-30 17:26:17 +0900

    [style] - Add layout for collection details page

--- src/collection-details.js
+++ src/collection-details.js
@@ -2,19 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const selectedImage = JSON.parse(localStorage.getItem("selectedImage"));
 
   if (selectedImage) {
-    const image = document.getElementById("collection-image");
-    const title = document.getElementById("collection-title");
-    const description = document.getElementById("collection-description");
+    // 해당 요소들을 id로 선택
+    const image = document.querySelector("#collection-image");
+    const title = document.querySelector("#collection-title");
+    const date = document.querySelector("#collection-date");
+    const description = document.querySelector("#collection-description");
 
-    // console.log(selectedImage);
+    console.log(selectedImage);
 
     image.src = selectedImage.imgSrc;
     image.alt = selectedImage.title;
+
     title.innerText = selectedImage.title;
+    date.innerHTML = selectedImage.date;
     description.innerText = selectedImage.description || "설명이 없습니다.";
   } else {
     const message = document.createElement("p");
     message.innerHTML = "이미지가 선택되지 않았습니다.";
-    document.getElementById("collection-section").appendChild(message);
+    document.querySelector("section").appendChild(message);
   }
 });
